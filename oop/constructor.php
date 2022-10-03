358944f930a064f9c4f3666ce629cd6fdb007a0f
Rizal <farabirizal111@gmail.com>
2022-10-03 22:17:20 +0700

    Menyelesaikan Kodingan Video ke 4 Revisi 3

--- oop/constructor.php
+++ oop/constructor.php
@@ -1,17 +1,16 @@
 <?php
 
 class Produk {
-public $judul = "judul",
-       $penulis = "penulis",
-       $penerbit = "penerbit",
-       $harga = 0;
+public $judul,
+       $penulis,
+       $penerbit,
+       $harga;
 
-       public function __construct($judul, $penulis, $penerbit, $harga){
+       public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) {
             $this->judul = $judul;
             $this->penulis = $penulis;
             $this->penerbit = $penerbit;
             $this->harga = $harga;
-
        }
 
        public function getLabel() {
@@ -22,16 +21,10 @@ public $judul = "judul",
 
 
 
-$produk3 = new Produk("Naruto","Masashi Kishimoto","Shonen Jump","30000");
-
-
-$produk4 = new Produk("Uncharted","Masashi Kishimoto","Shonen Jump","30000");
-
-$produk4->judul = "Uncharted";
-$produk4->penulis = "Neil Druckman";
-$produk4->penerbit = "Sony Computer";
-$produk4->harga = "250000";
-
-echo "Komik : " . $produk3->getLabel();
+$produk1 = new Produk("Naruto","Masashi Kishimoto","Shonen Jump","30000");
+$produk2 = new Produk("Uncharted","Neil Duckman","Sony Computer","250000");
+$produk3 = new Produk("Dragon Ball");
+echo "Komik : " . $produk1->getLabel();
 echo "<br>";
-echo "Game : " . $produk4->getLabel();
+echo "Game : " . $produk2->getLabel();
+var_dump($produk3);
