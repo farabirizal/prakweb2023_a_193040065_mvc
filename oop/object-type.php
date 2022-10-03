f5d0331cf0b3c3a2e626f3a353afa5ce295c0560
Rizal <farabirizal111@gmail.com>
2022-10-03 22:29:54 +0700

    menyelesaikan codingan video ke 5

--- oop/object-type.php
+++ oop/object-type.php
@@ -1,17 +1,16 @@
 <?php
 
 class Produk {
-public $judul = "judul",
-       $penulis = "penulis",
-       $penerbit = "penerbit",
-       $harga = 0;
+public $judul,
+       $penulis,
+       $penerbit,
+       $harga;
 
-       public function __construct($judul, $penulis, $penerbit, $harga){
+       public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) {
             $this->judul = $judul;
             $this->penulis = $penulis;
             $this->penerbit = $penerbit;
             $this->harga = $harga;
-
        }
 
        public function getLabel() {
@@ -21,13 +20,19 @@ public $judul = "judul",
 
 
 
+class CetakInfoProduk {
+    public function cetak (Produk $produk) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
 
 $produk1 = new Produk("Naruto","Masashi Kishimoto","Shonen Jump","30000");
-$produk2 = new Produk("Uncharted","Neil Druckman","Sony Computer","250000");
-$produk3 = new Produk("Dragon Ball");
+$produk2 = new Produk("Uncharted","Neil Duckman","Sony Computer","250000");
 
 echo "Komik : " . $produk1->getLabel();
 echo "<br>";
 echo "Game : " . $produk2->getLabel();
 echo "<br>";
-var_dump($produk3);
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
