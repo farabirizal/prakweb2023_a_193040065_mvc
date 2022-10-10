3f3196b7db38c0f9d473959744eb99660c23488b
Rizal <farabirizal111@gmail.com>
2022-10-10 23:12:29 +0700

    menambahkan_phpmvc_tugas4

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prak_pw_193040065_A/mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
---
+++ mvc/app/views/home/index.php
@@ -0,0 +1,10 @@
+<div class="container">
+  <div class="jumbotron mt-4">
+  <img src="<?= BASEURL; ?>/img/zal2.jpg" alt="Rizal Muhammad Farabi" width="350" height="300" class="rounded-circle shadow">
+    <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+    <p class="lead">Semoga senantiasa sehat</p>
+    <hr class="my-4">
+    
+    <a class="btn btn-primary btn-lg" href="" role="button">Jangan Lupa makan cilok</a>
+  </div>
+</div>
